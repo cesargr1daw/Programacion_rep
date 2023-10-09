15b62e65e96b870a964dabc0abfce6a1cd11f166
César García Ramos <114393134+cesargr1daw@users.noreply.github.com>
2023-10-09 18:27:25 +0200

    Create Ejercicio1.py

---
+++ 1º trimestre/UD1Iteradores While/Ejercicio1.py
@@ -0,0 +1,15 @@
+"""1. Para el siguiente ejercicio, calcula cuánto valdrá al final de la ejecución la
+
+variable producto . Realiza la traza de ejecución del programa para saber el
+
+valor de cada variable. Piensa la solución sin ejecutar la respuesta
+
+n = int(input("Ingrese un número entero positivo: "))
+producto = 1
+i = 1
+while i <= n:
+producto *= i
+i += 1
+print("El producto de los números del 1 al", n, "es:", producto)
+"""
+#El programa imprimirá: "El producto de los números del 1 al 5 es: 120".
