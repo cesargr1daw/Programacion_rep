758f8282775cc1eea11e5de7b30e76494897c2e8
César García Ramos <114393134+cesargr1daw@users.noreply.github.com>
2023-10-09 18:30:20 +0200

    Create Ejercicio2_while.py

---
+++ 1º trimestre/UD1Iteradores While/Ejercicio2_while.py
@@ -0,0 +1,13 @@
+"""
+2. Para el siguiente ejercicio, calcula cuánto valdrá al final de la ejecución la
+variable suma. Realiza la traza de ejecución del programa para saber el valor
+de cada variable. Piensa la solución sin ejecutar la respuesta
+N = 6
+suma = 0
+i = 1
+while i <= N:
+if i % 2 == 0:
+suma += i
+i += 1
+"""
+#El valor final de "suma" será 12.
