8bf548b61fd6e4b008e0c91c9fbdd7e202251ffa
César García Ramos <114393134+cesargr1daw@users.noreply.github.com>
2023-10-10 17:40:13 +0200

    Update Ejercicio3.py

--- 1º trimestre/EjerciciosForConsolidacion/Ejercicio3.py
+++ 1º trimestre/EjerciciosForConsolidacion/Ejercicio3.py
@@ -7,6 +7,5 @@ tantos * por fila como ese número. Por ejemplo, para número = 4
 * * * *
 """
 printar=int(input("Introduce el número que va a printar: "))
-cadena="*"*printar
-for i in range(1,printar+1):
-    print(cadena)
+for i in range(printar):
+    print("*" * printar)
