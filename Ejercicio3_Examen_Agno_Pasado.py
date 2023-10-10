bbd0e1874757201489536b7f995a916091b37d55
César García Ramos <114393134+cesargr1daw@users.noreply.github.com>
2023-10-10 18:24:41 +0200

    Add files via upload

--- Ejercicio3_Examen_Agno_Pasado.py
+++ Ejercicio3_Examen_Agno_Pasado.py
@@ -1,42 +1,35 @@
-tamañoEmpresa=int(input("Introduce el número de trabajadores tiene la empresa: "))
-python=0
-java=0
-totalP=tamañoEmpresa
-while 0<totalP:
-    lenguaje=input("Introduce el lenguaje que usas: ")
-    lenguaje=lenguaje.islower()
-    while not(lenguaje=="python" or lenguaje=="java"):
-        lenguaje=input("Introduce el lenguaje que usas: ")
-        lenguaje=lenguaje.islower()
-    match lenguaje:
-        case "python":
-            hombresP=0
-            mujeresP=0
-            edadTotal=0
-            sexo=input("Introduce si eres hombre o mujer (H o M): ")
-            sexo=sexo.islower()
-            edad=int(input("Introduce tu edad:"))
-            edadTotal+=edad
-            if sexo=="h":
-                hombresP+=1
-            else:
-                mujeresP+=1
-            python+=1
+tamagnoEmpresa = int(input("Introduce el número de trabajadores que tiene la empresa: "))
+python = 0
+java = 0
+hombresP = 0
+mujeresP = 0
+hombresJ = 0
+mujeresJ = 0
+edadTotalP = 0
+edadTotalJ = 0
 
-        case "java":
-            hombresJ=0
-            mujeresJ=0
-            edadTotal=0
-            sexo=input("Introduce si eres hombre o mujer (H o M): ")
-            sexo=sexo.islower()
-            edad=int(input("Introduce tu edad:"))
-            edadTotal+=edad
-            if sexo=="h":
-                hombresJ+=1
-            else:
-                mujeresJ+=1
-            java+=1
-    totalP-=1
+for i in range(tamagnoEmpresa):
+    lenguaje = input("Introduce el lenguaje que usas (python o java): ").lower()
+    while lenguaje != "python" and lenguaje != "java":
+        lenguaje = input("Introduce un lenguaje válido (python o java): ").lower()
 
-print(f"El {python*100/tamañoEmpresa} de empleados utiliza python, de los cuales {hombresP} son hombres y {mujeresP} mujeres y su edad media {edadTotal/(mujeresP+hombresP)}")
-print(f"El {java*100/tamañoEmpresa} de empleados utiliza java, de los cuales {hombresJ} son hombres y {mujeresJ} mujeres y su edad media {edadTotal/(mujeresJ+hombresJ)}")
+    sexo = input("Introduce si eres hombre o mujer (H o M): ").lower()
+    edad = int(input("Introduce tu edad: "))
+    
+    if lenguaje == "python":
+        edadTotalP += edad
+        if sexo == "h":
+            hombresP += 1
+        else:
+            mujeresP += 1
+        python += 1
+    elif lenguaje == "java":
+        edadTotalJ += edad
+        if sexo == "h":
+            hombresJ += 1
+        else:
+            mujeresJ += 1
+        java += 1
+
+print(f"El {python*100/tamagnoEmpresa} de empleados utiliza python, de los cuales {hombresP} son hombres y {mujeresP} mujeres y su edad media {edadTotalP/(mujeresP+hombresP)}")
+print(f"El {java*100/tamagnoEmpresa} de empleados utiliza java, de los cuales {hombresJ} son hombres y {mujeresJ} mujeres y su edad media {edadTotalJ/(mujeresJ+hombresJ)}")
