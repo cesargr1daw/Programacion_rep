fa06f6502706ef200d441cfb53a0fcbe63e21901
César García Ramos <114393134+cesargr1daw@users.noreply.github.com>
2023-10-15 16:35:41 +0200

    Create Ejercicio1.py

---
+++ 1º trimestre/EjerciciosForConsolidacion/Ejercicio1.py
@@ -0,0 +1,12 @@
+"""
+1. Calcula cuánto vale resultado siguiendo la traza de ejecución:
+n = 15
+resultado=0
+for i in range(n + 1):
+    if i % 2 == 0:
+        resultado = resultado +2 * i
+    else :
+        resultado = resultado +2 * i +1
+print(f"El resultado es:{resultado}")
+"""
+#El resultado final es 248
