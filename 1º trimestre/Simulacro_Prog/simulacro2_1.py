b6e5516c97bdd99291fe6e8d743e5759fbbffe0b
César García Ramos <114393134+cesargr1daw@users.noreply.github.com>
2023-10-17 10:11:35 +0200

    Actualizar simulacro2_1.py

--- 1º trimestre/Simulacro_Prog/simulacro2_1.py
+++ 1º trimestre/Simulacro_Prog/simulacro2_1.py
@@ -1,34 +1,39 @@
 print("*"*60)
 print("A) Añadir cliente")
-print(f"B) Mostrar los % de clientes premium y normales: ")
+print("B) Mostrar los % de clientes premium y normales: ")
 print("G) Salir")
 print("*"*60)
-opcion=input().upper()
-premium=0
-normales=0
-clientesT=premium+normales
-while opcion!="G":
+opcion = input().upper()
+premium = 0
+normales = 0
+
+while opcion != "G":
     match opcion:
         case "A":
-                vip=input("¿Desea ser Premium?: (S/N)").upper()
+            vip = input("¿Desea ser Premium?: (S/N)").upper()
+            print("Pulse G para salir")
+            while vip != "G":
+                if vip == "S" or vip == "N":
+                    if vip == "S":
+                        correo = input("Introduce el correo: ")
+                        premium += 1
+                    else:
+                        correo = input("Introduce el correo: ")
+                        normales += 1
+                vip = input("¿Desea ser Premium?: (S/N)").upper()
                 print("Pulse G para salir")
-                while vip!="G":
-                    if vip=="S" or vip=="N":
-                        if vip=="S":
-                            correo=input("Introduce el correo: ")
-                            premium+=1
-                        else:
-                            correo=input("Introduce el correo: ")
-                            normales+=1
-                    vip=input("¿Desea ser Premium?: (S/N)").upper()
-                    print("Pulse G para salir")
         case "B":
-            porc_Premium=premium*100/clientesT
-            porc_Normales=normales*100/clientesT
-            print(f"Hay {porc_Premium}% de usuarios premium y {porc_Normales}% de usuarios normales")
+            clientesT = premium + normales  # Debes calcular clientesT aquí
+            if clientesT > 0:
+                porc_Premium = (premium * 100) / clientesT
+                porc_Normales = (normales * 100) / clientesT
+                print(f"Hay {porc_Premium}% de usuarios premium y {porc_Normales}% de usuarios normales")
+            else:
+                print("No hay clientes registrados.")
+    
     print("*"*60)
     print("A) Añadir cliente")
     print("B) Mostrar los % de clientes premium y normales: ")
     print("G) Salir")
     print("*"*60)
-    opcion=input().upper()
+    opcion = input().upper()
