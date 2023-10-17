c93acefa24cfd7e3bf39f4cc4a02040da7b961a4
César García Ramos <114393134+cesargr1daw@users.noreply.github.com>
2023-10-17 10:14:09 +0200

    Actualizar simulacro2_2.py

--- 1º trimestre/Simulacro_Prog/simulacro2_2.py
+++ 1º trimestre/Simulacro_Prog/simulacro2_2.py
@@ -1,91 +1,81 @@
-print("*"*60)
+
+print("*" * 60)
 print("A) Añadir cliente")
-print(f"B) Mostrar los % de clientes premium y normales: ")
-print("G) Salir")
-print("*"*60)
-opcion=input().upper()
-premium=0
-normales=0
-clientesT=premium+normales
-mejor_clienteP=""
-mejor_clienteN=""
-producto_caroP=""
-precio_maxCP=0
-precio_maxCN=0
-art_maxCaroP=0
-precio_TotalP=0
-precio_TotalN=0
-cod_art_maxCaroP=""
-prod_max_vendido=0
-mas_vendido=""
-while opcion!="G":
+print("B) Mostrar los % de clientes premium y normales: ")
+print("C) Mostrar información de clientes premium")
+print("D) Mostrar información de clientes normales")
+print("E) Salir")
+print("*" * 60)
+opcion = input().upper()
+premium = 0
+normales = 0
+clientesT = premium + normales
+precio_maxCP = 0
+precio_maxCN = 0
+mejor_clienteP = ""
+mejor_clienteN = ""
+producto_caroP = ""
+producto_caroN = ""
+precioP = 0
+precioN = 0
+
+while opcion != "E":
     match opcion:
         case "A":
-                vip=input("¿Desea ser Premium?: (S/N)").upper()
-                print("Pulse G para salir")
-                while vip!="G":
-                    if vip=="S" or vip=="N":
-                        if vip=="S":
-                            correo=input("Introduce el correo: ")
-                            premium+=1
-                            pedido=int(input("Introduce el codigo del articulo: "))
-                            unidad=int(input("Indica cuántas unidades: "))
-                            precio_unidad=float(input("Introduce el precio por unidad: "))
-                            while pedido>=0:
-                                pedido=input("Introduce el codigo del articulo: ")
-                                unidad=int(input("Indica cuántas unidades: "))
-                                precio_unidad=float(input("Introduce el precio por unidad: "))
-                                precio_TotalP+=(unidad*precio_unidad)
-                                if precio_maxCP<(precio_TotalP):
-                                    mejor_clienteP=correo
-                                    producto_caroP=unidad
-                                    precio_maxCP=precio_TotalP
-                                if art_maxCaroP<precio_unidad:
-                                    cod_art_maxCaroP=pedido
-                                    precioP=precio_unidad
-                                 
-                        else:
-                            correo=input("Introduce el correo: ")
-                            normales+=1
-                            pedido=int(input("Introduce el codigo del articulo: "))
-                            unidad=int(input("Indica cuántas unidades: "))
-                            precio_unidad=float(input("Introduce el precio por unidad: "))
-                            while pedido>=0:
-                                pedido=input("Introduce el codigo del articulo: ")
-                                unidad=int(input("Indica cuántas unidades: "))
-                                precio_unidad=float(input("Introduce el precio por unidad: "))
-                                precio_TotalN+=(unidad*precio_unidad)
-                                if prod_max_vendido<unidad:
-                                    mas_vendido=pedido
-                                if precio_maxCP<(precio_TotalP):
-                                    mejor_clienteN=correo
-                                    producto_caroN=unidad
-                                    precio_maxCN=precio_TotalN
-
-
-                    vip=input("¿Desea ser Premium?: (S/N)").upper()
-                    print("Pulse G para salir")
+            vip = input("¿Desea ser Premium?: (S/N)").upper()
+            print("Pulse E para salir")
+            while vip != "E":
+                if vip == "S" or vip == "N":
+                    if vip == "S":
+                        correo = input("Introduce el correo: ")
+                        premium += 1
+                        pedido = int(input("Introduce el codigo del articulo: "))
+                        unidad = int(input("Indica cuántas unidades: "))
+                        precio_unidad = float(input("Introduce el precio por unidad: "))
+                        while pedido >= 0:
+                            precioP += unidad * precio_unidad
+                            if precio_unidad > precio_maxCP:
+                                mejor_clienteP = correo
+                                producto_caroP = pedido
+                                precio_maxCP = precio_unidad
+                            pedido = int(input("Introduce el codigo del articulo: "))
+                            unidad = int(input("Indica cuántas unidades: "))
+                            precio_unidad = float(input("Introduce el precio por unidad: "))
+                    else:
+                        correo = input("Introduce el correo: ")
+                        normales += 1
+                        pedido = int(input("Introduce el codigo del articulo: "))
+                        unidad = int(input("Indica cuántas unidades: "))
+                        precio_unidad = float(input("Introduce el precio por unidad: "))
+                        while pedido >= 0:
+                            precioN += unidad * precio_unidad
+                            if unidad > producto_caroN:
+                                mejor_clienteN = correo
+                                producto_caroN = unidad
+                            pedido = int(input("Introduce el codigo del articulo: "))
+                            unidad = int(input("Indica cuántas unidades: "))
+                            precio_unidad = float(input("Introduce el precio por unidad: "))
+                vip = input("¿Desea ser Premium?: (S/N)").upper()
+                print("Pulse E para salir")
         case "B":
-            porc_Premium=premium*100/clientesT
-            porc_Normales=normales*100/clientesT
+            clientesT = premium + normales
+            porc_Premium = (premium * 100) / clientesT if clientesT > 0 else 0
+            porc_Normales = (normales * 100) / clientesT if clientesT > 0 else 0
             print(f"Hay {porc_Premium}% de usuarios premium y {porc_Normales}% de usuarios normales")
         case "C":
-            print(porc_Premium)
-            print(mejor_clienteP)
-            print(producto_caroP)
+            print(f"Mejor cliente premium: {mejor_clienteP}")
+            print(f"Producto más caro comprado por cliente premium: {producto_caroP}")
+            print(f"Precio máximo pagado por cliente premium: {precio_maxCP}")
         case "D":
-            print(porc_Normales)
-            print(mas_vendido)
-        case "E":
-            if precio_maxCN>precio_maxCP:
-                print(mejor_clienteN)
-                print(precio_maxCN)
-            else:
-                print(mejor_clienteP)
-                print(precio_maxCP)
-    print("*"*60)
+            print(f"Mejor cliente normal: {mejor_clienteN}")
+            print(f"Unidades más compradas por cliente normal: {producto_caroN}")
+            print(f"Precio total pagado por cliente normal: {precioN}")
+
+    print("*" * 60)
     print("A) Añadir cliente")
     print("B) Mostrar los % de clientes premium y normales: ")
-    print("G) Salir")
-    print("*"*60)
-    opcion=input().upper()
+    print("C) Mostrar información de clientes premium")
+    print("D) Mostrar información de clientes normales")
+    print("E) Salir")
+    print("*" * 60)
+    opcion = input().upper()
